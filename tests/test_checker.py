71d45f6e5e04da2d613cf6e8df9844b2f7c0858d
Russell <russell.tchamba@gmail.com>
2026-04-29 13:08:26 +0200

    change python version

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -5,8 +5,9 @@ from pytest_mock import MockerFixture
 
 from simple_http_checker.checker import check_urls
 
+
 def test_check_urls_success(mocker: MockerFixture):
-    
+
     # mock_requests_get = mocker.patch("simple_http_checker.checker.requests.get")
     mock_get = mocker.patch("simple_http_checker.checker.requests.get")
 
@@ -31,7 +32,7 @@ def test_check_urls_success(mocker: MockerFixture):
     # mock_get.side_effect = [response_200, response_404, timeout_error]
     mock_get.side_effect = [
         response_200,
-        response_404, 
+        response_404,
         timeout_error,
         connection_error,
         request_error,
@@ -52,8 +53,8 @@ def test_check_urls_success(mocker: MockerFixture):
     assert "Timeout" in results[urls[2]]
     assert "Connection Error" in results[urls[3]]
     assert "RequestException" in results[urls[4]]
-    
-    
+
+
 def test_check_urls_empty_list():
     urls = []
     results = check_urls(urls)
@@ -67,8 +68,8 @@ def test_check_urls_customer_timeout(mocker: MockerFixture):
     response_200.reason = "OK"
     response_200.ok = True
     mock_get.return_value = response_200
-    #timeout_error = requests.exceptions.Timeout()
-    #mock_get.side_effect = timeout_error
+    # timeout_error = requests.exceptions.Timeout()
+    # mock_get.side_effect = timeout_error
 
     urls = ["http://example.com"]
     custom_timeout = 10
